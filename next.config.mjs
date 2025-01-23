e88c108159a0b45d1571bd37f8de82f7632c8fdb
Leo <minarae@emart.com>
2025-01-23 16:54:21 +0900

    기본 설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
       },
     ]
   },
+  output: 'export',  // Static HTML 내보내기 설정
+  images: {
+    unoptimized: true  // 정적 내보내기를 위한 이미지 최적화 비활성화
+  }
 };
 
 export default nextConfig;
